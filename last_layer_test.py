9448101c326eb44b0fe3a8c66a10a2cbda716b17
Joanp131 <Joanp131@users.noreply.github.com>
2018-11-06 13:20:22 +0100

    13:20 - 6/11/2018

--- last_layer_test.py
+++ last_layer_test.py
@@ -1,15 +1,11 @@
 import numpy as np
 import math as m
+import time as t
 
-hidden=0
-weights=0
-
-def main():
-    hidden = np.matrix('3;2;8;-5')
-
-    #Weights are labeled from output to hidden
-    weights = np.random.rand(2,4)
-
+"""
+Feedforward
+"""
+time1 = t.time()
 def CrossEntropy(p, y):
     return -(y*m.log10(p) + (1-y)*m.log(1-p))
 
@@ -22,6 +18,19 @@ def softmax(X):
     exps = np.exp(X)
     return exps / np.sum(exps)
 
-main()
+
+hidden = np.matrix('3;2;8;-5')
+weights = np.random.rand(2,4)
+
 feedforward(hidden, weights)
-#printOUT()
+
+time2 = t.time()
+totaltime = round((time2 - time1)*1000, 2)
+print("Time for feedforward: ", totaltime, "millis")
+
+"""
+Backpropagation
+"""
+ans = np.matrix('1;0')
+print("Ans: ", ans)
+
