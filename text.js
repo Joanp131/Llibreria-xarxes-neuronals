ae9b6c5c131ae7a774717bece8c2edc3e2d58ef5
Joanp131 <Joanp131@users.noreply.github.com>
2018-11-13 16:26:21 +0100

    16:26 - 13/11/2018

--- text.js
+++ text.js
@@ -1,7 +1,13 @@
+let tables = [
+	"weights1_table"
+]
+
 function weights1 () {
 	document.getElementById("weights1_table").style.display = "inline";
 }
 
 function clearText () {
-	document.getElementsByClassName("table").style.display = "none";
+	for(let i = 0; i < tables.length; i++) {
+		document.getElementById(tables[i]).style.display = "none";
+	}
 }
