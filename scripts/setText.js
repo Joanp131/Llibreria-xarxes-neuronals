a6133ce8bd532a7fc65155579e0157a3ea0a2088
Joanp131 <Joanp131@users.noreply.github.com>
2018-11-30 16:46:52 +0100

    16:46 - 30/11/2018

--- scripts/setText.js
+++ scripts/setText.js
@@ -1,44 +1,38 @@
-
+let ids = ["titleText", "llenguatge", "runBut", "trainBut", "resultText", "atention", "warn", "coll0but"]
 
 function catala() {
 
 	$.getJSON("scripts/json/text_cat.json", function(cat) {
-	    console.log(cat); // this will show the info it in firebug console
-	    document.getElementById("titleText").innerHTML = cat["titleText"]
-		document.getElementById("llenguatge").innerHTML = cat["llenguatge"]
-		document.getElementById("runBut").innerHTML = cat["runBut"]
-		document.getElementById("trainBut").innerHTML = cat["trainBut"]
-		document.getElementById("resultText").innerHTML = cat["resultText"]
-		document.getElementById("atention").innerHTML = cat["atention"]
-		document.getElementById("runBut").innerHTML = cat["runBut"]
-		document.getElementById("warn").innerHTML = cat["warn"]
+	    //console.log(cat); 
+	    for (let i = 0; i < ids.length; i++) {
+	    	document.getElementById(ids[i]).innerHTML = cat[ids[i]]
+	    }
 	});
-	
+
+
 }
 
 function castella() {
 	
 	$.getJSON("scripts/json/text_cas.json", function(cas) {
-	    console.log(cas); // this will show the info it in firebug console
-	    document.getElementById("titleText").innerHTML = cas["titleText"]
-		document.getElementById("llenguatge").innerHTML = cas["llenguatge"]
-		document.getElementById("runBut").innerHTML = cas["runBut"]
-		document.getElementById("trainBut").innerHTML = cas["trainBut"]
-		document.getElementById("resultText").innerHTML = cas["resultText"]
-		document.getElementById("atention").innerHTML = cas["atention"]
-		document.getElementById("runBut").innerHTML = cas["runBut"]
-		document.getElementById("warn").innerHTML = cas["warn"]
+		// console.log("funciona?")
+	 //    console.log(cas); 
+	    for (let i = 0; i < ids.length; i++) {
+	    	document.getElementById(ids[i]).innerHTML = cas[ids[i]]
+	    }
 	});
+
 
 }
 
 function angles() {
-	document.getElementById("titleText").innerHTML = angles_text["titleText"]
-	document.getElementById("llenguatge").innerHTML = angles_text["llenguatge"]
-	document.getElementById("runBut").innerHTML = angles_text["runBut"]
-	document.getElementById("trainBut").innerHTML = angles_text["trainBut"]
-	document.getElementById("resultText").innerHTML = angles_text["resultText"]
-	document.getElementById("atention").innerHTML = angles_text["atention"]
-	document.getElementById("runBut").innerHTML = angles_text["runBut"]
-	document.getElementById("warn").innerHTML = angles_text["warn"]
+	$.getJSON("scripts/json/text_en.json", function(en) {
+	    //console.log(en); 
+	    for (let i = 0; i < ids.length; i++) {
+	    	document.getElementById(ids[i]).innerHTML = en[ids[i]]
+	    }
+	});
+
+	
+
 }
